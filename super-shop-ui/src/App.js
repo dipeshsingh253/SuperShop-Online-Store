746790e1e1591b46ad70f90f576a4b9e3840ddc4
dipeshsingh253 <sinhdipesh@gmail.com>
2022-12-06 16:26:12 +0530

    login page added

--- super-shop-ui/src/App.js
+++ super-shop-ui/src/App.js
@@ -1,11 +1,16 @@
-import Root from "./routes/root";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import RegistrationForm from "./components/RegistrationFrom";
+import LoginFrom from "./components/LoginFrom";
+
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/register",
     element: <RegistrationForm />,
   },
+  {
+    path: "/login",
+    element: <LoginFrom/>,
+  }
 ]);
 
 function App() {
--- super-shop-ui/src/services/UserService.js
+++ super-shop-ui/src/services/UserService.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-// const USER_BASE_URL = 
+// const USER_BASE_URL =
 
-class UserService{
+class UserService {
+  saveUser(user) {
+    return axios.post("http://localhost:8088/register", user);
+  }
 
-    saveUser(user){
-        return axios.post("http://localhost:8088/register",user);
-    }
-
+  loginUser(userDto) {
+    return axios.post("http://localhost:8088/login", userDto);
+  }
 }
 
 export default new UserService();
